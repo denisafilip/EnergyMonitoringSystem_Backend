01bc002575852dc0693feed0eb075d11c38bbb50
denisafilip <denisafilip0207@gmail.com>
2022-10-17 23:01:58 +0300

    feature: Add energy platform models for Postgres database

--- DS2022_30441_Filip_Denisa_1_Backend/settings.py
+++ DS2022_30441_Filip_Denisa_1_Backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-du%#3buavqkgz52%fxemqz=a$y+l+2#30ip9+&8hlz7*0ley(4'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'energy_platform',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'DS2022_30441_Filip_Denisa_1_Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
 
--- energy_platform/models.py
+++ energy_platform/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN"
+        CLIENT = "CLIENT"
+    role = models.CharField(choices=Role.choices, max_length=10, default=Role.CLIENT)
+
+    def __str__(self):
+        return f"{self.email}"
+
+
+class Device(models.Model):
+    description = models.CharField(max_length=500)
+    address = models.CharField(max_length=200)
+    max_hourly_consumption = models.IntegerField()
+
+    def __str__(self):
+        return f"Description: {self.description}, from {self.address}, consumes {self.max_hourly_consumption} per hour."
+
+
+class UserToDevice(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    device_id = models.ForeignKey(Device, on_delete=models.CASCADE)
+
+
+class Consumption(models.Model):
+    mapping_id = models.ForeignKey(UserToDevice, on_delete=models.CASCADE)
+    consumption = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
